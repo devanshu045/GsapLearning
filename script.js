d6ab66d1468ca47b0f10035df89ae410fe7fba87
devanshu045 <Sharmadev9664431714@gmail.com>
2024-10-13 02:53:27 +0530

    timeline

---
+++ script.js
@@ -0,0 +1,16 @@
+// gsap.to("#div1",{
+//     duration:5,
+//     delay:2,
+//     x : 120,
+//     y : 120,
+//     repeat:3
+// })
+gsap.from("#div1 h1",{
+    opacity:0,
+    x : 120,
+    y : 120,
+    duration:5,
+    delay:2,
+    repeat:3,
+    stagger:1
+})
